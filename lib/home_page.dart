e2c724782d8c531e4709b3cfe4969e7f0390612f
LapasovSh25 <shahzodlapasov25@gmail.com>
2023-08-01 23:07:17 +0500

    first commit

---
+++ lib/home_page.dart
@@ -0,0 +1,220 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<String> _listItem = [
+    'assets/images/two.jpg',
+    'assets/images/three.jpg',
+    'assets/images/four.jpg',
+    'assets/images/five.jpg',
+    'assets/images/one.jpg',
+    'assets/images/two.jpg',
+    'assets/images/three.jpg',
+    'assets/images/four.jpg',
+    'assets/images/five.jpg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[600],
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        // leading: const Icon(Icons.menu),
+        title: const Text("Home"),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              width: 36,
+              height: 30,
+              decoration: BoxDecoration(
+                  color: Colors.grey[800],
+                  borderRadius: BorderRadius.circular(10)),
+              child: const Center(child: Text("0")),
+            ),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[800],
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/one.jpg'),
+                      fit: BoxFit.cover),
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(20)),
+              child: Center(
+                child: Text(
+                  "Furniture UI",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Home',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Settings',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.help,
+                color: Colors.white,
+              ),
+              title: Text(
+                'Help',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onTap: () {},
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height*0.5,
+            ),
+            ListTile(
+             
+              title: Text(
+                "Developed by Lapasov_Sh",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                height: 250,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    image: const DecorationImage(
+                        image: AssetImage('assets/images/one.jpg'),
+                        fit: BoxFit.cover)),
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      gradient:
+                          LinearGradient(begin: Alignment.bottomRight, colors: [
+                        Colors.black.withOpacity(.4),
+                        Colors.black.withOpacity(.2),
+                      ])),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      const Text(
+                        "Lifestyle Sale",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 35,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        height: 50,
+                        margin: const EdgeInsets.symmetric(horizontal: 40),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: Center(
+                            child: Text(
+                          "Shop Now",
+                          style: TextStyle(
+                              color: Colors.grey[900],
+                              fontWeight: FontWeight.bold),
+                        )),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Expanded(
+                  child: GridView.count(
+                crossAxisCount: 2,
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                children: _listItem
+                    .map((item) => Card(
+                          color: Colors.transparent,
+                          elevation: 0,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                image: DecorationImage(
+                                    image: AssetImage(item),
+                                    fit: BoxFit.cover)),
+                            child: Transform.translate(
+                              offset: const Offset(50, -50),
+                              child: Container(
+                                margin: const EdgeInsets.symmetric(
+                                    horizontal: 65, vertical: 63),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.white),
+                                child: const Icon(
+                                  Icons.bookmark_border,
+                                  size: 15,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ))
+                    .toList(),
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
